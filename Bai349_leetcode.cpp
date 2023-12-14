68ecc5268a0e64dce9029327123ae425e7590d03
Xuan Diep <diepp031005@gmail.com>
2023-12-14 09:26:32 +0700

    Update code

--- Bai349_leetcode.cpp
+++ Bai349_leetcode.cpp
@@ -6,27 +6,27 @@ void input_nums (int nums[], int size) {
     }
 }
 
-void mangGiaoDiem (int MangGiaoDiem[], int nums1[], int size1, int nums2[], int size2) {
+void arr_Intersection (int Arr_Intersection[], int nums1[], int size1, int nums2[], int size2) {
     int count = 0;
     for (int i = 0; i < size1; i++) {
         for (int j = 0; j < size2; j++) {
             if (nums1[i] == nums2[j]) {
                 count++;
-                MangGiaoDiem[count-1] = nums1[i];
+                Arr_Intersection[count-1] = nums1[i];
             }
         }
     }
     
     if (count == 0) 
-        printf("2 mang da nhap khong co phan tu chung !");
+        printf("Array intersection has no element !");
     else if (count == 1)
-        printf("%d ", MangGiaoDiem[0]);
+        printf("%d ", Arr_Intersection[0]);
     else {
-        printf("%d ", MangGiaoDiem[0]);
+        printf("%d ", Arr_Intersection[0]);
         for (int i = 1; i < count; i++) {
             int temp = 0;
             for (int j = 0; j < i; j++) {
-                if (MangGiaoDiem[i] == MangGiaoDiem[j]) {
+                if (Arr_Intersection[i] == Arr_Intersection[j]) {
                     temp++;
                     continue;
                 }
@@ -34,18 +34,18 @@ void mangGiaoDiem (int MangGiaoDiem[], int nums1[], int size1, int nums2[], int 
                     continue;   
             }
             if (temp == 0)
-                printf("%d ", MangGiaoDiem[i]);
+                printf("%d ", Arr_Intersection[i]);
         }
     }
 }
 int main() {
     int size1, size2;
-    printf("Nhap so phan tu cua mang thu 1: "); scanf("%d", &size1);
+    printf("Enter the number of elements in 1st array: "); scanf("%d", &size1);
     int nums1[size1];
     input_nums(nums1, size1);
-    printf("Nhap so phan tu cua mang thu 2: "); scanf("%d", &size2);
+    printf("Enter the number of elements in 2st array: "); scanf("%d", &size2);
     int nums2[size2];
     input_nums(nums2, size2);
-    int MangGiaoDiem[10];
-    mangGiaoDiem(MangGiaoDiem, nums1, size1, nums2, size2);
+    int Arr_Intersection[size1 + size2];
+    arr_Intersection(Arr_Intersection, nums1, size1, nums2, size2);
 }
